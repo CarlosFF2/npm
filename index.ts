f31cde0faf8abb482235ef3b60a836e5e7ee3bb8
CarlosFF2 <a3520110156@alumno.uttehuacan.edu.mx>
2023-10-03 18:12:03 -0600

    primer commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+const express = require("express")
+
+
+const example = function(){
+    console.log('===============siuuu==================');
+    
+    console.log('============siuuuuu=================');
+}
+
+const app = express();
+app.listen(
+
+    9000,
+    example
+
+
+    
+)
